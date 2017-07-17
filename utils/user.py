a19333443b06aa9f5cc906aa5270b8fda6f81990
danibe248 <danibe248@gmail.com>
2017-07-17 22:07:22 +0200

    simulation con retweets e protofollow

--- utils/user.py
+++ utils/user.py
@@ -2,108 +2,112 @@ import numpy as np
 
 
 class User:
-	def __init__(self, node_id, topic):
-		#node['id'] = self.id
-		self.id = node_id			  
-		self.attachment = {}
-		#self.node = node
+    def __init__(self, node_id, topic):
+        #node['id'] = self.id
+        self.id = node_id
+        self.attachment = {}
+        #self.node = node
 
-		#node['user'] = self
-		
-		##### personal interest
-		pi = np.random.random_integers(100, size=(1, topic))[0].tolist()
-		sumpi = sum(pi)
-		fpi = map(lambda x: float(x)/sumpi, pi)
-		self.pi = fpi
-		self.pi_average = np.average(fpi)
+        #node['user'] = self
 
-		##### timezone (refer to documentation)
-		tz = np.zeros(12)
-		i = np.random.randint(13) + 4
-		i = i % 12
+        # personal interest
+        pi = np.random.random_integers(100, size=(1, topic))[0].tolist()
+        sumpi = sum(pi)
+        fpi = map(lambda x: float(x) / sumpi, pi)
+        self.pi = fpi
+        self.pi_average = np.average(fpi)
 
-		## low activity
-		for k in range(4):
-		    tz[(k + i) % 12] = np.random.triangular(0, 0.25, 0.6)
-		i = (i + 4) % 12
-		
-		## high activity
-		for k in range(4):
-		    tz[(k + i) % 12] = np.random.triangular(0.4, 0.75, 1)
+        # timezone (refer to documentation)
+        tz = np.zeros(12)
+        i = np.random.randint(13) + 4
+        i = i % 12
 
-		self.tz = tz.tolist()
-		self.followers = []
-		self.followings = []
-		self.dtag = []
-		
-		interest = []
-		for j in range(topic):
-		    if self.pi[j] > 0.5:
-		        interest.append(j)
-		self.interest = interest
+        # low activity
+        for k in range(4):
+            tz[(k + i) % 12] = np.random.triangular(0, 0.25, 0.6)
+        i = (i + 4) % 12
 
-	def generate_fov(self, time, tweets, retweets):
-	   	f = len(self.followings)
-		#t = len(tweets)
-		#r = len(retweets)
+        # high activity
+        for k in range(4):
+            tz[(k + i) % 12] = np.random.triangular(0.4, 0.75, 1)
 
-		# Generate tweets
-		usrs_choice = np.random.choice(f + 1, f/4)
-		time_choice = np.random.choice(time + 1, f/4)
-		
-		twts = []
-		for i in range(f/4):
-			twts.append(tweets[usrs_choice[i], \
-							   time_choice[i]])
+        self.tz = tz.tolist()
+        self.followers = []
+        self.followings = []
+        self.dtag = []
 
-		# Generate retweets
-		usrs_choice = np.random.choice(f + 1, f/4)
-		time_choice = np.random.choice(time + 1, f/4)
+        interest = []
+        for j in range(topic):
+            if self.pi[j] > 0.5:
+                interest.append(j)
+        self.interest = interest
 
-		rtwts = []
-		# TODO
+    def generate_fov(self, time, tweets, retweets):
+        fings = len(self.followings)
+        fers = len(self.followers)
+        #t = len(tweets)
+        #r = len(retweets)
 
-		# Generate DTAGs
-		# TODO
-		dtags = []
+        # Generate tweets
+        #usrs_choice = np.random.choice(f + 1, f/4)
+        time_choice = np.random.choice(time + 1, 1)
 
-		
-		return (twts, rtwts, dtags)
+        twts = []
+        for i in range(fings):
+            for t in range(time_choice, time + 1):
+                try:
+                    twts.append(tweets[self.followings[i], t])
+                except:
+                    pass
 
-	def add_follower(self, follower):
-		self.followers.append(follower)
+        # Generate retweets
+        rtwts = []
+        for i in range(fings):
+            for t in range(time_choice, time + 1):
+                try:
+                    rtwts.append(retweets[self.followings[i], t])
+                except:
+                    pass
 
-	def add_following(self, following):
-		self.followings.append(following)
+        # Generate DTAGs
+        # TODO
+        dtags = []
 
-	def get_attachment(self, u):
-		try:
-			return self.attachment[u]
-		except:
-			return -1
+        return (twts, rtwts, dtags)
 
-	def get_dtag(self, time):
-		try:
-			return self.dtag[time]
-		except:
-			return None
+    def add_follower(self, follower):
+        self.followers.append(follower)
 
-	def add_dtag(self, time, tweet):
-		if self.get_dtag[time]:
-			self.dtag[time].append(tweet)
-		else:
-			self.dtag[time] = [tweet]
+    def add_following(self, following):
+        self.followings.append(following)
 
-		
-	def __str__(self):
-		stri = ''
-		stri += ('User id: %d\n' % self.id)
-		stri += 'PI:\n'
-		stri += str(self.pi)
-		stri += '\nTZ:\n'
-		stri += str(self.tz)
-		stri += '\nFollowers:\n'
-		stri += str(self.followers)
-		stri += '\nFollowings:\n'
-		stri += str(self.followings)
-		return stri
+    def get_attachment(self, u):
+        try:
+            return self.attachment[u]
+        except:
+            return -1
+
+    def get_dtag(self, time):
+        try:
+            return self.dtag[time]
+        except:
+            return None
+
+    def add_dtag(self, time, tweet):
+        if self.get_dtag[time]:
+            self.dtag[time].append(tweet)
+        else:
+            self.dtag[time] = [tweet]
+
+    def __str__(self):
+        stri = ''
+        stri += ('User id: %d\n' % self.id)
+        stri += 'PI:\n'
+        stri += str(self.pi)
+        stri += '\nTZ:\n'
+        stri += str(self.tz)
+        stri += '\nFollowers:\n'
+        stri += str(self.followers)
+        stri += '\nFollowings:\n'
+        stri += str(self.followings)
+        return stri
